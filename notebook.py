6dcfe932f406513a6fdad508a8686ee57cde521e
Yannick GICQUEL <ygicquel@gmail.com>
2015-03-21 22:10:09 +0100

    new feature: initial open/save dialog boxes
    Signed-off-by: Yannick GICQUEL <ygicquel@gmail.com>

--- notebook.py
+++ notebook.py
@@ -69,6 +69,13 @@ class TextViewWindow(Gtk.Window):
 
         toolbar.insert(Gtk.SeparatorToolItem(), 10)
 
+        open_btn = Gtk.ToolButton.new_from_stock(Gtk.STOCK_OPEN)
+        open_btn.connect("clicked", self.on_open_clicked)
+        toolbar.insert(open_btn, 11)
+        save_btn = Gtk.ToolButton.new_from_stock(Gtk.STOCK_SAVE)
+        save_btn.connect("clicked", self.on_save_clicked)
+        toolbar.insert(save_btn, 12)
+
     def create_textview(self):
         scrolledwindow = Gtk.ScrolledWindow()
         scrolledwindow.set_hexpand(True)
@@ -78,9 +85,7 @@ class TextViewWindow(Gtk.Window):
         self.textview = Gtk.TextView()
         self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
         self.textbuffer = self.textview.get_buffer()
-        self.textbuffer.set_text("This is some text inside of a Gtk.TextView. "
-            + "Select text and click one of the buttons 'bold', 'italic', "
-            + "or 'underline' to modify the text accordingly.")
+        self.textbuffer.set_text("")
         scrolledwindow.add(self.textview)
 
         self.tag_bold = self.textbuffer.create_tag("bold",
@@ -103,6 +108,53 @@ class TextViewWindow(Gtk.Window):
 
     def on_justify_toggled(self, widget, justification):
         self.textview.set_justification(justification)
+
+    def on_open_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog("Please choose a file", self,
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("Text files")
+        filter_text.add_mime_type("text/plain")
+        dialog.add_filter(filter_text)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            selected_file = dialog.get_filename()
+            with open(selected_file, 'r') as f:
+                data = f.read()
+                self.textbuffer.set_text("")
+                emptyBuffer = self.textbuffer
+                format = emptyBuffer.register_deserialize_tagset()
+                self.textbuffer.deserialize(emptyBuffer,
+                    format, emptyBuffer.get_end_iter(), data)
+        elif response == Gtk.ResponseType.CANCEL:
+            dialog.destroy()
+
+        dialog.destroy()
+
+    def on_save_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog("Save file", self,
+        Gtk.FileChooserAction.SAVE,
+        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+         Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            save_file = dialog.get_filename()
+            start_iter = self.textbuffer.get_start_iter()
+            end_iter = self.textbuffer.get_end_iter()
+            format = self.textbuffer.register_serialize_tagset()
+            text = self.textbuffer.serialize(self.textbuffer,
+                format, start_iter, end_iter)
+            with open(save_file, 'w') as f:
+                f.write(text)
+        elif response == Gtk.ResponseType.CANCEL:
+            dialog.destroy()
+
+        dialog.destroy()
 
 win = TextViewWindow()
 win.connect("delete-event", Gtk.main_quit)

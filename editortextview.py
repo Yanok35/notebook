0e9ea358136882c7949db6d4debced58eea1ed86
Yannick GICQUEL <ygicquel@gmail.com>
2015-03-28 19:49:31 +0100

    editor: move to DrawingArea widget
    Signed-off-by: Yannick GICQUEL <ygicquel@gmail.com>

--- editortextview.py
+++ editortextview.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#-*- coding: utf8 -*-
 
-from gi.repository import Gdk, Gtk, GtkSource, Pango
+# Unicode support
+from __future__ import unicode_literals
+
+import cairo
+from gi.repository import Gdk, Gtk, Pango, PangoCairo
 
 from editortextbuffer import EditorTextBuffer
 
@@ -22,23 +27,30 @@ class EditorTextView(Gtk.ScrolledWindow):
 
         self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
 
-        self.textview = GtkSource.View()
-        #self.textview = Gtk.TextView()
-        self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        vp = Gtk.Viewport()
+        darea = Gtk.DrawingArea()
+        darea.set_size_request(800, 200)
+        darea.set_can_focus(True)
 
-        fontdesc = Pango.FontDescription("Monospace 11")
-        self.textview.modify_font(fontdesc)
-        self.textview.set_show_line_numbers(True)
-        self.textview.set_highlight_current_line(True)
+        darea.connect('draw', self.on_draw)
+        events  = Gdk.EventMask.ENTER_NOTIFY_MASK
+        events |= Gdk.EventMask.KEY_PRESS_MASK
+        darea.set_events(events)
+        darea.connect('enter-notify-event', self.on_enter_notify_event)
+        darea.connect('key-press-event', self.on_key_press_event)
+#        darea.connect_after('realize', self.on_darea_realize)
+##        vp.add(darea)
+        #self.darea = vp
+        self.darea = darea
+        self.darea.show()
 
-        ## Following works on Gtk.TextView only.
-        #self.style_context = self.textview.get_style_context()
-        #self.default_bg_color = self.style_context.get_background_color(Gtk.StateFlags.NORMAL)
-        #self.textview.override_background_color(Gtk.StateFlags.NORMAL,
-        #                                        Gdk.RGBA(0, 0, 0, 1))
-
-        self.textview.connect('key-press-event', self.on_key_press_event)
-        self.textview.show()
+#        w = darea.get_root_window()
+#        events = w.get_events()
+#        print events
+##        events |= Gdk.EventMask.ENTER_NOTIFY_MASK
+#        events |= Gdk.EventMask.ALL_EVENTS_MASK
+#        print events
+#        w.set_events(events)
 
         img = Gtk.Image.new_from_file("oshw-logo-800-px.png")
         vp = Gtk.Viewport()
@@ -47,9 +59,23 @@ class EditorTextView(Gtk.ScrolledWindow):
         self.image.show()
 
         self.add(self.image)
+        
+        self.txt_tst_buf = ""
 
-    def on_justify_toggled(self, widget, justification):
-        self.textview.set_justification(justification)
+##     def on_darea_realize(self, widget):
+##         print ("realize", widget)
+
+##         w = self.darea.get_root_window()
+##         events = w.get_events()
+## #        print events
+##         events |= Gdk.EventMask.ENTER_NOTIFY_MASK
+## #        events |= Gdk.EventMask.ALL_EVENTS_MASK
+## #        print events
+##         w.set_events(events)
+#        self.darea.connect('key-press-event', self.on_key_press_event)
+#        self.darea.connect('enter-notify-event', self.on_enter_notify_event)
+        self.darea.connect('focus-in-event', self.on_focus_in_event)
+        self.darea.grab_focus()
 
     def subdoc_new(self, docid):
         self.textbuffers[docid] = EditorTextBuffer()
@@ -60,15 +86,15 @@ class EditorTextView(Gtk.ScrolledWindow):
         if visid == -1 and docid is not None:
             assert(self.textbuffers[docid] is not None)
             self.remove(self.image)
-            self.add(self.textview)
+            self.add(self.darea)
             self.textbuffer_visibleid = docid
         elif visid != -1 and docid is None:
-            self.remove(self.textview)
+            self.remove(self.darea)
             self.add(self.image)
             self.textbuffer_visibleid = -1
 
         if docid:
-            self.textview.set_buffer(self.textbuffers[docid])
+        #     self.textview.set_buffer(self.textbuffers[docid])
             self.textbuffer_visibleid = docid
 
     def subdoc_load_from_file(self, docid, filename):
@@ -86,11 +112,36 @@ class EditorTextView(Gtk.ScrolledWindow):
         assert(self.textbuffers[docid] is not None)
         return self.textbuffers[docid].get_content_as_text()
 
+    def on_draw(self, widget, ctx):
+        print(ctx)
+        ctx.set_source_rgb(0, 0, 0)
+        ctx.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
+            cairo.FONT_WEIGHT_NORMAL)
+        ctx.set_font_size(12)
+        ctx.move_to(10, 20)
+        #ctx.show_text("This is hello")
+        ctx.show_text(self.txt_tst_buf)
+
+    def on_enter_notify_event(self, widget, event):
+        print("notify")
+        widget.grab_focus()
+        
+    def on_focus_in_event(self, widget, event):
+        print("focusin")
+        widget.grab_focus()
+
     def on_key_press_event(self, window, event):
         key = Gdk.keyval_name(event.keyval)
 
         # debug string :
-        #print(key + str(event.state))
+        print(key + str(event.state))
+        print(event.keyval)
+        
+        #if event.keyval in range(ord('a'), ord('z')):
+        self.txt_tst_buf += key
+        self.darea.queue_draw()
+
+        return True
 
         if event.state & Gdk.ModifierType.CONTROL_MASK and key in ('o', 's', 'v'):
             if key == 'v':

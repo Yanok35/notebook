b4a03228587b3de311cf8d99bdb9a340ddc4a992
Yannick GICQUEL <ygicquel@gmail.com>
2015-04-12 19:31:58 +0200

    projecttreeview: move to MVC design (on-going)

--- projecttreeview.py
+++ projecttreeview.py
@@ -11,6 +11,7 @@ from gi.repository import Gdk, GObject, Gtk, Pango
 from lxml import etree
 
 #from editortextview import EditorTextView
+from projecttreestore import ProjectTreeStore
 
 class PTreeView(Gtk.TreeView):
     def __init__(self):
@@ -24,11 +25,6 @@ class ProjectTreeView(Gtk.Box):
     __gtype_name__ = str("ProjectTreeView")
 
     __gsignals__ = {
-        str('subdoc-inserted'): (GObject.SIGNAL_RUN_FIRST, None, (int,)),
-        str('subdoc-deleted'): (GObject.SIGNAL_RUN_FIRST, None, (int,)),
-        str('subdoc-changed'): (GObject.SIGNAL_RUN_FIRST, None, (int,)),
-        str('subdoc-load-from-file'): (GObject.SIGNAL_RUN_FIRST, None, (int, str)),
-        str('subdoc-save-to-file'): (GObject.SIGNAL_RUN_FIRST, None, (int, str)),
         str('subdoc-order-changed'): (GObject.SIGNAL_RUN_FIRST, None, ()), # list of ids to retreive
         str('subdoc-selection-changed'): (GObject.SIGNAL_RUN_FIRST, None, ()),
 
@@ -44,13 +40,10 @@ class ProjectTreeView(Gtk.Box):
         self.set_hexpand(True)
         self.set_vexpand(True)
 
-        self.treestore = Gtk.TreeStore(GObject.TYPE_STRING, GObject.TYPE_INT)
-        self.sigid_row_inserted = self.treestore.connect("row-inserted", self.on_treemodel_row_inserted)
-        self.sigid_row_deleted = self.treestore.connect("row-deleted", self.on_treemodel_row_deleted)
-        self.sigid_row_changed = self.treestore.connect("row-changed", self.on_treemodel_row_changed)
+        self.treestore = None #ProjectTreeStore()
 
         self.treeview = PTreeView()
-        self.treeview.set_model(self.treestore)
+        #self.treeview.set_model(self.treestore)
         self.treeview.expand_all()
         self.treeview.set_reorderable(True)
         self.treeview.set_activate_on_single_click(True)
@@ -103,127 +96,22 @@ class ProjectTreeView(Gtk.Box):
 
         self.subdocs_id = 0
 
-    def rec_treestore_set_docs(self, iter, elem, filesdir):
+    def set_model(self, model):
+        self.treeview.set_model(model)
+        self.treestore = model
 
-        #print(type(elem.text), elem.text.__class__.__name__)
-        if elem.text.__class__.__name__ != 'unicode':
-            name = unicode(elem.text, encoding='utf-8')
-        else:
-            name = elem.text
-        docid = int(elem.attrib["id"])
-        iter_elem = self.treestore.append(iter, [elem.text, docid])
-        #print (elem.text)
-
-        # Create a new subdocument from file
-        filename = os.path.join(filesdir, str(docid) + ".subdoc")
-        self.emit('subdoc-inserted', docid)
-        self.emit('subdoc-load-from-file', docid, str(filename))
-
-        # Parse node's child if they exists
-        subdoc_list = elem.findall('subdoc')
-        for subdoc in subdoc_list:
-            self.rec_treestore_set_docs(iter_elem, subdoc, filesdir)
+    def get_model(self):
+        return self.treestore
 
     def load_from_file(self, filename):
-        #print("user asked to load project from ", filename)
-        #print(os.path.basename(filename))
-        doc = etree.parse(filename)
-
-        # todo: check file is in good format...
-        # Check files dir exists
-        filesdir = filename + ".files"
-        if not os.path.exists(filesdir):
-            raise ValueError
-
-        self.treestore.handler_block(self.sigid_row_inserted)
-        self.treestore.handler_block(self.sigid_row_deleted)
-        self.treestore.handler_block(self.sigid_row_changed)
-        self.treeselection.handler_block(self.sigid_treeselection_changed)
-        
-        # Remove previous doc in treeview
-        self.treestore.clear()
-        # Remove subdoc refs
-        self.subdocs_id = 0
-
-        # Parse XML file to add node recursively
-        doctree = doc.find('doctree')
-        self.subdocs_id = int(doctree.attrib["subdocs_id"])
-        subdoc_list = doctree.findall('subdoc')
-        for subdoc in subdoc_list:
-            self.rec_treestore_set_docs(None, subdoc, filesdir)
-
-        self.treeselection.handler_unblock(self.sigid_treeselection_changed)
-        self.treestore.handler_unblock(self.sigid_row_changed)
-        self.treestore.handler_unblock(self.sigid_row_deleted)
-        self.treestore.handler_unblock(self.sigid_row_inserted)
-
-        # Parse XML file to find selection list
-        docid_list = []
-        selnode = doc.find('selection')
-        subdoc_sel_list = selnode.findall('docid')
-        for docid in subdoc_sel_list:
-            docid_list.append(int(docid.text))
-
-        if len(docid_list) > 0:
-            self.set_selection_list(docid_list)
-
-        if len(subdoc_list) > 0:
-            self.emit('subdoc-order-changed')
-
+        docid_list = self.treestore.load_from_file(filename)
+        self.set_selection_list(docid_list)
         self.treeview.expand_all()
-
-    def rec_treestore_get_docs(self, iter, elem, filesdir):
-        docname = unicode(self.treestore.get_value(iter, 0), encoding='utf-8')
-        docid = unicode(self.treestore.get_value(iter, 1))
-
-        node = etree.SubElement(elem, 'subdoc')
-        node.text = docname
-        node.attrib["id"] = docid
-
-        # Catch content of subdoc[docid] and save to file
-        filename = os.path.join(filesdir, str(docid)+".subdoc")
-        print("doc id ", docid, " save to ", filename)
-        self.emit('subdoc-save-to-file', int(docid), str(filename))
-
-        for i in range(0, self.treestore.iter_n_children(iter)):
-            child = self.treestore.iter_nth_child(iter, i)
-            self.rec_treestore_get_docs(child, node, filesdir)
-
-        return node
 
     def save_to_file(self, filename):
         print("user asked to save project to ", filename)
-
-        filesdir = filename + ".files"
-        print(filesdir)
-        if not os.path.exists(filesdir):
-            os.mkdir(filesdir)
-
-        projet = etree.Element('notebook_project')
-        doc = etree.ElementTree(projet)
-
-        treeview_node = etree.SubElement(projet, 'doctree')
-        #treeview_node.text = "fichier 1"
-        treeview_node.attrib["subdocs_id"] = unicode(self.subdocs_id)
-
-        iter = self.treestore.get_iter_first()
-        while iter is not None and self.treestore.iter_is_valid(iter):
-            self.rec_treestore_get_docs(iter, treeview_node, filesdir)
-            iter = self.treestore.iter_next(iter)
-
         sel_list = self.get_selection_list()
-        sel_node = etree.SubElement(projet, 'selection')
-        if sel_list:
-            for docid in sel_list:
-                node = etree.SubElement(sel_node, 'docid')
-                node.text = str(docid)
-
-        outfile = open(filename, 'w')
-        doc.write(outfile, pretty_print=True)
-
-    def _get_new_docid(self):
-        self.subdocs_id += 1
-        return self.subdocs_id
+        self.treestore.save_to_file(filename, sel_list)
 
     def get_selection_list(self):
         retlist = []
@@ -235,34 +123,8 @@ class ProjectTreeView(Gtk.Box):
         return retlist
         #return self.current_sel_list
 
-    def rec_find_subdociter_from_id(self, iter, docid):
-        #docname = unicode(self.treestore.get_value(iter, 0), encoding='utf-8')
-        elem_docid = int(self.treestore.get_value(iter, 1))
-        if elem_docid == docid:
-            return iter
-
-        for i in range(0, self.treestore.iter_n_children(iter)):
-            child = self.treestore.iter_nth_child(iter, i)
-            child_iter = self.rec_find_subdociter_from_id(child, docid)
-            if child_iter:
-                return child_iter
-
-        return None
-
-    def find_subdociter_from_id(self, docid):
-        elem_iter = None
-
-        iter = self.treestore.get_iter_first()
-        while iter is not None and self.treestore.iter_is_valid(iter):
-            elem_iter = self.rec_find_subdociter_from_id(iter, docid)
-            if elem_iter is not None:
-                break
-            iter = self.treestore.iter_next(iter)
-
-        return elem_iter
-
     def set_selection(self, docid):
-        iter = self.find_subdociter_from_id(docid)
+        iter = self.treestore.find_subdociter_from_id(docid)
         if iter:
             self.treeselection.select_iter(iter)
 
@@ -280,72 +142,6 @@ class ProjectTreeView(Gtk.Box):
         #self.current_sel_list = self.treeselection
         self.treeselection.emit('changed')
 
-    def rec_treestore_get_doc_list(self, iter):
-        retlist = []
-
-        docname = unicode(self.treestore.get_value(iter, 0), encoding='utf-8')
-        docid = int(self.treestore.get_value(iter, 1))
-
-        retlist.append([docid, docname])
-
-        for i in range(0, self.treestore.iter_n_children(iter)):
-            child = self.treestore.iter_nth_child(iter, i)
-            childlist = self.rec_treestore_get_doc_list(child)
-            for child in childlist:
-                retlist.append(child)
-
-        return retlist
-
-    def get_doc_list(self):
-        retlist = []
-
-        iter = self.treestore.get_iter_first()
-        while iter is not None and self.treestore.iter_is_valid(iter):
-            childlist = self.rec_treestore_get_doc_list(iter)
-
-            for child in childlist:
-                retlist.append(child)
-
-            iter = self.treestore.iter_next(iter)
-
-        #print(retlist)
-        return retlist
-
-    def get_docid_list(self):
-        retlist = []
-        doclist = self.get_doc_list()
-        for doc in doclist:
-            retlist.append(doc[0])
-        return retlist
-
-
-    def rec_get_docid_level(self, iter, docid):
-
-        curid = int(self.treestore.get_value(iter, 1))
-        if curid == docid:
-            return self.treestore.iter_depth(iter)
-
-        level = -1
-        for i in range(0, self.treestore.iter_n_children(iter)):
-            child = self.treestore.iter_nth_child(iter, i)
-            level = self.rec_get_docid_level(child, docid)
-            if level != -1:
-                break
-
-        return level
-
-    def get_docid_level(self, docid):
-
-        level = -1
-        iter = self.treestore.get_iter_first()
-        while iter is not None and self.treestore.iter_is_valid(iter):
-            level = self.rec_get_docid_level(iter, docid)
-            if level != -1:
-                return level
-            iter = self.treestore.iter_next(iter)
-
-        return level
-
     def on_button_add_doc_clicked(self, widget):
         treesel = self.treeview.get_selection()
         store, sel_list = treesel.get_selected_rows()
@@ -356,26 +152,7 @@ class ProjectTreeView(Gtk.Box):
             path = sel_list[0]
             row_pos = int(str(path)[str(path).rfind(':') + 1:])
 
-        #print ("add new doc at", str(path))
-        #print ("row_pos", str(row_pos))
-
-        parent = None
-        if len(sel_list) == 1:
-            iter_sel = self.treestore.get_iter(path)
-            superpath = path.copy()
-            if superpath.up() and superpath.get_depth() > 0:
-                parent = self.treestore.get_iter(superpath)
-
-        iter_new = self.treestore.insert(parent, row_pos+1)
-
-        # Create a new subdocument
-        docid = self._get_new_docid()
-
-        # Add an entry in project's treeview
-        self.treestore.set_value(iter_new, 0, "<Write your title>")
-        self.treestore.set_value(iter_new, 1, docid)
-        self.emit('subdoc-inserted', docid)
-        #self.emit('subdoc-changed', docid)
+        self.treestore.subdoc_add_new(path, row_pos)
 
     def on_button_del_doc_clicked(self, widget):
         treesel = self.treeview.get_selection()
@@ -385,14 +162,7 @@ class ProjectTreeView(Gtk.Box):
             return
 
         for path in sel_list:
-            print ("remove doc at", str(path))
-            iter = self.treestore.get_iter(path)
-            docid = self.treestore.get_value(iter, 1)
-            self.treestore.remove(iter)
-            self.emit('subdoc-deleted', docid)
-            #self.emit('subdoc-changed', docid)
-            # warning: after remove is called, all path in sel_list
-            # are not valid anymore... to fixup
+            self.treestore.subdoc_del(path)
 
     def on_button_export_doc_clicked(self, widget):
         self.emit('project-export')
@@ -402,15 +172,6 @@ class ProjectTreeView(Gtk.Box):
 
     def on_button_export_to_html_clicked(self, widget):
         self.emit('export-to-html')
-
-    def on_treemodel_row_inserted(self, treemodel, path, iter):
-        print ("row-inserted", str(path), iter)
-
-    def on_treemodel_row_deleted(self, treemodel, path):
-        print ("row-deleted", str(path))
-
-    def on_treemodel_row_changed(self, treemodel, path, iter):
-        print ("row-changed", str(path), iter)
 
     def on_treeview_drag_begin(self, widget, context):
         store, path_list = self.treeselection.get_selected_rows()
@@ -469,11 +230,8 @@ class ProjectTreeView(Gtk.Box):
     def on_treeview_cell_edited(self, cell_renderer, path, new_text):
         new_text = unicode(new_text, encoding='utf-8')
         print ("cell edited", str(path), " => ", new_text)
-        iter = self.treestore.get_iter(path)
-        self.treestore.set_value(iter, 0, new_text)
 
-        docid = int(self.treestore.get_value(iter, 1))
-        self.emit('subdoc-changed', docid)
+        self.treestore.subdoc_set_title(path, new_text)
 
-        #treestore = self.treeview.get_model()
+GObject.type_register(ProjectTreeView)
 
